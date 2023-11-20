295b03ffbbe559b317c7fefc8926da131a2bd53b
KaanAy14 <147263463+KaanAy14@users.noreply.github.com>
2023-11-20 21:45:50 +0300

    pyramid.java

--- yildiz3.java
+++ yildiz3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 //You can print a pyramid via using this code segment below.
-public class yildiz3 {
+public class pyramid {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
